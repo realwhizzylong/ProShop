bc049c4190f31f6ec1f3b08e2aa25a09d70f2a84
realwhizzylong <therealwhizzy@gmail.com>
2021-04-19 14:32:27 +0100

    add upload image file

--- front-end/src/screens/ProductEditScreen.js
+++ front-end/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ match, history }) => {
     const [brand, setBrand] = useState('');
     const [countInStock, setCountInStock] = useState(0);
     const [description, setDescription] = useState('');
+    const [uploading, setUploading] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -44,6 +46,26 @@ const ProductEditScreen = ({ match, history }) => {
             }
         }
     }, [dispatch, success, productId, product, history])
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image', file);
+        setUploading(true);
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+            const { data } = await axios.post('/api/upload', formData, config);
+            setImage(data);
+            setUploading(false);
+        } catch (error) {
+            console.error(error);
+            setUploading(false);
+        }
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -93,6 +115,8 @@ const ProductEditScreen = ({ match, history }) => {
                                 value={image}
                                 onChange={e => setImage(e.target.value)}
                             />
+                            <Form.File id="image-file" label="Choose file" custom onChange={uploadFileHandler} />
+                            {uploading && <Loader />}
                         </Form.Group>
                         <Form.Group controlId="category">
                             <Form.Label>Category</Form.Label>
